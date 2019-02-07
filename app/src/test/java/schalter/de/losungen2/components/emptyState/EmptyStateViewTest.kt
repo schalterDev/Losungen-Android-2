d07d6b09c7b711b370a31863c20ac8738eb71e54
Martin Schalter <schalter.dev@gmail.com>
2019-02-07 13:54:35 +0100

    :rotating_light: :construction: add tests for empty state view

--- app/src/test/java/schalter/de/losungen2/components/emptyState/EmptyStateViewTest.kt
+++ app/src/test/java/schalter/de/losungen2/components/emptyState/EmptyStateViewTest.kt
@@ -2,20 +2,22 @@ package schalter.de.losungen2.components.emptyState
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import org.hamcrest.Matchers.equalTo
-import org.junit.Assert.assertNotNull
-import org.junit.Assert.assertThat
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows
 import org.robolectric.annotation.Config
-import org.robolectric.shadows.ShadowDialog
 import schalter.de.losungen2.R
 import schalter.de.losungen2.utils.TestApplication
+
 
 @RunWith(RobolectricTestRunner::class)
 @Config(application = TestApplication::class)
@@ -24,28 +26,50 @@ class EmptyStateViewTest {
     private lateinit var context: Context
     private lateinit var emptyStateView: EmptyStateView
 
+    private val title = R.string.monthly_verse_title
+    private val icon = R.drawable.ic_cloud_off
+    private val buttonText = R.string.import_new_data
+
+    // TODO add test with custom attributes in xml
+
     @Before
     fun loadView() {
         context = getApplicationContext()
+
+        Robolectric.buildAttributeSet()
+
         emptyStateView = LayoutInflater
                 .from(context)
                 .inflate(R.layout.empty_state, EmptyStateView(context), true)
                 as EmptyStateView
+
+        emptyStateView.setButtonText(buttonText)
+        emptyStateView.setIcon(icon)
+        emptyStateView.setTitle(title)
     }
 
     @Test
     fun buttonShouldShowAndInteract() {
-        emptyStateView.setButtonText(R.string.import_new_data)
-
         val button: Button = emptyStateView.findViewById(R.id.emptyStateImport)
-        assertThat(button.text as String, equalTo(context.getString(R.string.import_new_data)))
+        assertThat(button.text as String, equalTo(context.getString(buttonText)))
 
+        var buttonClicked = false
+        emptyStateView.onButtonClick { buttonClicked = true }
         button.performClick()
 
-        val dialog = ShadowDialog.getLatestDialog()
-        assertNotNull(dialog)
-        val dialogView = dialog.findViewById<View>(R.id.import_dialog_layout)
-        assertNotNull(dialogView)
+        assertTrue(buttonClicked)
     }
 
+    @Test
+    fun showTitle() {
+        val titleElement = emptyStateView.findViewById<TextView>(R.id.emptyStateTitle)
+        assertThat(titleElement.text as String, equalTo(context.getString(title)))
+    }
+
+    @Test
+    fun showIcon() {
+        val imageView = emptyStateView.findViewById<ImageView>(schalter.de.losungen2.R.id.emptyStateIcon)
+        val shadowDrawable = Shadows.shadowOf(imageView.drawable)
+        assertEquals(icon, shadowDrawable.createdFromResId)
+    }
 }

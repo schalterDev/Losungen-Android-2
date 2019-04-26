7cc08d4378e1280b3d55395ae0a2bcbd49841582
Martin Schalter <schalter.dev@gmail.com>
2019-04-26 17:44:35 +0200

    :zap: date in databases has to be unique

--- app/src/main/java/schalter/de/losungen2/dataAccess/monthly/MonthlyVerse.kt
+++ app/src/main/java/schalter/de/losungen2/dataAccess/monthly/MonthlyVerse.kt
@@ -8,7 +8,8 @@ import schalter.de.losungen2.dataAccess.Language
 import java.util.*
 
 @Entity(indices = [
-    Index(value = ["date", "language"], unique = true),
+//    Index(value = ["date", "language"], unique = true),
+    Index(value = ["date"], unique = true),
     Index(value = ["monthly_verse_id"])
 ])
 class MonthlyVerse(

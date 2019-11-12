19e63cd507d1075a2f94f651766175442ad1efca
Martin Schalter <schalter.dev@gmail.com>
2019-11-12 19:19:14 +0100

    better time converting test

--- app/src/test/java/de/schalter/losungen/dataAccess/DailyVerseTest.kt
+++ app/src/test/java/de/schalter/losungen/dataAccess/DailyVerseTest.kt
@@ -10,7 +10,7 @@ class DailyVerseTest {
     @Test
     fun timeConverting() {
         val calendar = Calendar.getInstance()
-        calendar.set(Calendar.HOUR_OF_DAY, 2)
+        calendar.set(2019, Calendar.JANUARY, 1, Calendar.HOUR_OF_DAY, 0)
 
         val dateAt2 = calendar.time
         val verse = DailyVerse(
@@ -22,17 +22,13 @@ class DailyVerseTest {
                 oldTestamentVerseText = ""
         )
 
-        val calendarUTC = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
-        calendarUTC.set(Calendar.YEAR, calendar.get(Calendar.YEAR))
-        calendarUTC.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR))
-        calendarUTC.set(Calendar.HOUR_OF_DAY, 12)
-        calendarUTC.set(Calendar.MINUTE, 0)
-        calendarUTC.set(Calendar.SECOND, 0)
-        calendarUTC.set(Calendar.MILLISECOND, 0)
+        val utcCalendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+        utcCalendar.set(2019, Calendar.JANUARY, 1, 12, 0, 0)
+        utcCalendar.set(Calendar.MILLISECOND, 0)
 
-        assertThat(verse.date, equalTo(calendarUTC.time))
+        assertThat(verse.date, equalTo(utcCalendar.time))
 
         verse.date = dateAt2
-        assertThat(verse.date, equalTo(calendarUTC.time))
+        assertThat(verse.date, equalTo(utcCalendar.time))
     }
 }
